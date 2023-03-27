e872760d9a80ab82f5fae98974a3b5f1bad2d11c
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-03-27 19:05:51 +0000

    Add email function

--- student.py
+++ student.py
@@ -1,7 +1,8 @@
 from datetime import date, timedelta
 
+
 class Student:
-    """ A student class as base for method testing"""
+    """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
@@ -10,9 +11,13 @@ class Student:
         self.end_date = date.today() + timedelta(days=365)
         self.naughty_list = False
 
-    
     @property
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
 
-        
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+    def alert_santa(self):
+        self.naughty_list = True
